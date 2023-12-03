e2823f1e1a0cdf731df2c14c0a28a08d56ae1463
Kevin Kinner <kevin-kinner@web.de>
2023-12-03 19:08:12 +0100

    File reader returns null even though it hast a value

--- 2023/DataReader/script.js
+++ 2023/DataReader/script.js
@@ -1,12 +1,41 @@
-//Reads txt document line by line prints out data for easier conversion to array.
-document.getElementById("inputfile").addEventListener("change", function () {
-	var fr = new FileReader();
-	fr.onload = function () {
-		let text = '["' + fr.result.replace(/\n/g, '","') + '"]';
-		text =  text.replace(/\s/g, "")
-		document.getElementById("output").textContent =
-			text
-	};
+async function changeAlgorithm(srciptToRun){
+	let key = document.getElementById("scriptPicker").value;
+	let text = null;
+	switch (key) {
+		case "LineBreakTextToArray":
+				text = await lineBreakTextToArray();				
+				console.log(text);
+			break;
+	
+		default:
+			alert("There is something wrong with the selected refactorer")
+			break;
+	}
+	if (text != null) {
+		await appendToOutput(text)	
+	}else{
+		alert("No readable text file")
+	}
+	
+}
 
-	fr.readAsText(this.files[0]);
-});
+async function lineBreakTextToArray() {
+	let fileValue = document.getElementById("inputfile").files[0]
+	let fr = new FileReader();
+	let test; 
+	fr.onload=function() {
+		text = '["' + fr.result.replace(/\n/g, '","') + '"]';
+		text =  text.replace(/\s/g, "");
+		test = fr.result;
+	}
+	fr.readAsText(fileValue)
+	
+	await appendToOutput(fr.result)
+	
+	//fr does have a result property that holds the data that i need but fr.result is null ...
+}
+
+
+async function appendToOutput(text) {
+	document.getElementById("output").append(text);
+}
