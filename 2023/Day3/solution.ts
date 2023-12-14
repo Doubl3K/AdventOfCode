632980d2508b5ec1038d8cb43737372e84527745
Kevin Kinner <kevin.kinner@css.de>
2023-12-14 12:42:28 +0100

    ahhahh

--- 2023/Day3/solution.ts
+++ 2023/Day3/solution.ts
@@ -1,28 +1,41 @@
-const Read = require('file-reader');
+const Read = require("file-reader");
 //
 
 console.log("Script is running");
-const fileContent: String = Read.file("testData.txt");
-const lineArray: String[] = fileContent.split("\r\n");
-let charArray: String[];
-let line1: String;
-let line2: String;
-let line3: String;
+const fileContent: string = Read.file("testData.txt");
+const lineArray: string[] = fileContent.split("\r\n");
+let charArray1: string[];
+let charArray2: string[];
+let charArray3: string[];
 
-for (let i = 0; i < lineArray.length; i++) {
-    lineArray[i].substring(0, lineArray.length);
+for (let i = 0; i < lineArray.length - 2; i++) {
+	charArray1 = lineArray[i].split("");
+	charArray2 = lineArray[i + 1].split("");
+	charArray3 = lineArray[i + 2].split("");
+	yolo(charArray1, charArray2, charArray3);
 }
 
+function yolo(
+	charArray1: string[],
+	charArray2: string[],
+	charArray3: string[]
+) {
+	let number: string = "";
 
+	let tempKeep: string = "";
+	for (let i = 0; i < charArray1.length; i++) {
+		if (parseInt(charArray1[i])) {
+		}
+	}
 
-
-
+	console.log(tempKeep);
+}
 
 /*TODO: 
 Read File content ✅
-Substring lines
-Add substringArray of line to textArray
-Loop through line Array to check for digit ->
+Substring lines✅
+Add substringArray of line to textArray ✅
+Loop through char Array to check for digit ->
 if next index != digit Then number = digits
 Check if any digit has symbol around it (NaN && !"."):
     Get the 8 values on positions around digit
@@ -30,12 +43,4 @@ Check if any digit has symbol around it (NaN && !"."):
     ***
     *5*
     ***
-*/
-
-
-/* 
-Jetzt ist nathürlich hier die Frage.
-Brauch ich in dem Falls nen Itterator oder eher nicht so.
-Eigentlich schon weil ich muss ja das LineArray eins hoch und eins runter gehen.
-Also macht warscheinlich ein for hier mehr sinn als ein foreach
 */
